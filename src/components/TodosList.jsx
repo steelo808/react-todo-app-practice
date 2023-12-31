c0259f27e474c2523965fe88cfac3a1d10b1368a
Sterling Newsom <sterlingnewsom808@gmail.com>
2023-12-30 19:26:15 -0500

    created react components and data model

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import TodoItem from '../components/TodoItem';
 
-function TodosList(props) {
-  const { todosProps } = props; // destructuring the prop object
+function TodosList({ todosProps }) {
+   // destructuring the prop object
   return (
     <ul>
       {todosProps.map((todo) =>(
-        <li>{todo.title}</li>
+        <TodoItem key={todo.id} itemProp={todo} />
       ))}
     </ul>
   );
