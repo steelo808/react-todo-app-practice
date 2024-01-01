81da959c099efb1afdc9b06f673d4b94df510618
Sterling Newsom <sterlingnewsom808@gmail.com>
2023-12-31 21:56:14 -0500

    using the useRef Hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Navbar() {
     const [dropdown , setDropdown] = useState(false);
+    const ref = useRef();
+    console.log(ref);
+
+    useEffect(() =>{
+        document.title = `Current state value: ${dropdown}`;
+    }, [dropdown]);
   return (
     <nav>
       <ul>
@@ -10,10 +16,13 @@ function Navbar() {
             <li>About</li>
             <li>
                 <button onClick={()=> setDropdown(!dropdown)}>Services <span>&#8595;</span></button>
-                <ul>
+                {dropdown && (
+                    <ul>
                     <li>Design</li>
                     <li>Developoment</li>
                 </ul>
+                )}
+                
             </li>
       </ul>
     </nav>
